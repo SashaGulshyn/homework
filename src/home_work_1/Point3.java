e288f16e3d228d129f642ee3ded7f27db2a521ef
Sasha Gulshyn <kessedione@gmail.com>
2023-04-19 15:16:04 +0300

    Добавил задание 3

---
+++ src/home_work_1/Point3.java
@@ -0,0 +1,18 @@
+package src.home_work_1;
+
+public class Point3 {
+    public static void main(String[] args) {
+        int y = 12;
+        int x = 14;
+        System.out.println(Math.abs(-1));
+        System.out.println(Math.pow(4,2));
+        System.out.println(Math.pow(5,3));
+        System.out.println(Math.sqrt(16));
+        System.out.println(Math.round(12.5));
+        System.out.println(Math.floor(33.6));
+        System.out.println(Math.ceil(612.4));
+        System.out.println(Math.max(y,x));
+        System.out.println(Math.min(y,x));
+        System.out.println(Math.tan(180));
+    }
+}
