87572bf6a621bd8c4626aedf9a2f8f599396512a
Sasha Gulshyn <kessedione@gmail.com>
2023-04-19 13:59:43 +0300

    Модифицировал 1.3 и добавил задачу 2

--- src/home_work_1/Point1_3.java
+++ src/home_work_1/Point1_3.java
@@ -2,8 +2,8 @@ package src.home_work_1;
 
 public class Point1_3 {
     public static void main(String[] args) {
-        double x=42.5f; //использую double для дробного числа - 101010.1 в двоичной системе счисления
-        int y=125; //1111101 в двоичной системе счисления
-        System.out.println(x>>y); //float или double не могут быть применены при побитовых операциях
+        double x=101010.1; //использую double для дробного числа - 101010.1 в двоичной системе счисления
+        double y=1111101; //1111101 в двоичной системе счисления
+        System.out.println(x&&y); //float или double не могут быть применены при побитовых операциях
     }
 }
