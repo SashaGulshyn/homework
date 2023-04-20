d070b851bac385ad9b55d0f1d86cb033d265b3cc
Sasha Gulshyn <kessedione@gmail.com>
2023-04-20 12:15:17 +0300

    Добавил задачу номер 4.1

---
+++ src/home_work_1/Point4_1.java
@@ -0,0 +1,13 @@
+package src.home_work_1;
+
+public class Point4_1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " - чётное число");
+            } else {
+                System.out.println(i + " - нечётное число");
+            }
+        }
+    }
+}
