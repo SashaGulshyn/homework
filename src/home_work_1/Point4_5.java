d53f4c0fe78848f016ce5a57cac81791ee337a55
Sasha Gulshyn <kessedione@gmail.com>
2023-04-20 16:43:29 +0300

    Добавил задачу 4.5

---
+++ src/home_work_1/Point4_5.java
@@ -0,0 +1,8 @@
+package src.home_work_1;
+
+public class Point4_5 {
+    public static void main(String[] args) {
+        char ch=102;
+        System.out.println(ch+ "- красивая буква");
+    }
+}
