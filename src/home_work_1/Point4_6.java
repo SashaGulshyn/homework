88b7d05cc54dda23d04a8d3d9038b0ef57627c4f
Sasha Gulshyn <kessedione@gmail.com>
2023-04-20 16:55:21 +0300

    Добавил задачу 4.6

---
+++ src/home_work_1/Point4_6.java
@@ -0,0 +1,12 @@
+package src.home_work_1;
+
+public class Point4_6 {
+    public static void main(String[] args) {
+        int Y = 2023;
+        if (Y % 4 == 0) {
+            System.out.println(Y + "- високосный год");
+        } else {
+            System.out.println(Y + "- этот год не високосный");
+        }
+    }
+}
