f014f6ae26df9a0be7be6622fb123f94d3bbf9d6
Sasha Gulshyn <kessedione@gmail.com>
2023-04-20 12:26:23 +0300

    Добавил задание 4.2

---
+++ src/home_work_1/Point4_2.java
@@ -0,0 +1,11 @@
+package src.home_work_1;
+
+public class Point4_2 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 8;
+        int c = 5;
+        int d = (a + b + c)/3;
+        System.out.println(d + "- среднее арифметическое из 15");
+    }
+}
