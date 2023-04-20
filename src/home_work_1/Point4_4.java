f6ae9021a400c99536b52702c9d7b445d4bba3f8
Sasha Gulshyn <kessedione@gmail.com>
2023-04-20 16:30:32 +0300

    Добавил задачу 4.4

---
+++ src/home_work_1/Point4_4.java
@@ -0,0 +1,8 @@
+package src.home_work_1;
+
+public class Point4_4 {
+    public static void main(String[] args) {
+    int a = 2;
+        System.out.println(Math.pow(a,10)+"- количество байт в одном килобайте");
+    }
+}
