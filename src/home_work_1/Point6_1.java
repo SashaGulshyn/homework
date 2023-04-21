4bff126e3a3bf7ccffef7581edce889324e73bcb
Sasha Gulshyn <kessedione@gmail.com>
2023-04-21 13:36:09 +0300

    Добавил в задание 6.1 Objects.equals

--- src/home_work_1/Point6_1.java
+++ src/home_work_1/Point6_1.java
@@ -1,11 +1,14 @@
 package src.home_work_1;
 import java.util.Scanner;
+import java.util.Objects;
 
 public class Point6_1 {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        System.out.print("Введите имя");
-        if (sc.hasNext("Вася")) {
+       Object a = (sc.findInLine("Вася"));
+       Object b = "Вася";
+        if (Objects.equals(a,b)) {
             System.out.println("Привет!\nЯ тебя так долго ждал ");
         }else{
             System.out.println("");
