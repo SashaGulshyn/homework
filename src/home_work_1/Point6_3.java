e2a4ac08a930496fff487b30b99435b1205b3f30
Sasha Gulshyn <kessedione@gmail.com>
2023-04-21 16:26:42 +0300

    Добавил switch в задание 6.3

--- src/home_work_1/Point6_3.java
+++ src/home_work_1/Point6_3.java
@@ -4,13 +4,18 @@ import java.util.Scanner;
 public class Point6_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String name;
         System.out.print("Введите имя");
-        if (sc.hasNext("Анастасия")) {
-            System.out.println("Я тебя так долго ждал ");
-        }else if (sc.hasNext("Вася")) {
-            System.out.println("Привет!\nЯ тебя так долго ждал");
-        }else if (sc.hasNextLine()){
-            System.out.println("Добрый день,а вы кто?");
+        name = sc.nextLine();
+        switch (name){
+            case "Вася":
+                System.out.println("Привет! Я тебя так долго ждал");
+                break;
+            case "Анастасия":
+                System.out.println("Я тебя так долго ждал");
+                break;
+            default:
+                System.out.println("Добрый день, а вы кто?");
         }
     }
 }
