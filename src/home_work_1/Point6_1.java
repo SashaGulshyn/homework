d0fc06cf3026af6b0f6449671a6730d1df270311
Sasha Gulshyn <kessedione@gmail.com>
2023-04-21 12:16:42 +0300

    Исправил код для задания 6.1 с Васей

--- src/home_work_1/Point6_1.java
+++ src/home_work_1/Point6_1.java
@@ -5,9 +5,10 @@ public class Point6_1 {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        System.out.print("Введите имя");
-        if (sc.hasNextLine()) {
-            String name = sc.findInLine("Вася");
+        if (sc.hasNext("Вася")) {
             System.out.println("Привет!\nЯ тебя так долго ждал ");
+        }else{
+            System.out.println("");
         }
     }
 }
