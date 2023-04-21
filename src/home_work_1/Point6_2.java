fa968f739650d2b7adef0562c6a83ddf70889699
Sasha Gulshyn <kessedione@gmail.com>
2023-04-21 13:37:28 +0300

    Добавил if else if + Objects.equals)

--- src/home_work_1/Point6_2.java
+++ src/home_work_1/Point6_2.java
@@ -1,12 +1,19 @@
 package src.home_work_1;
 import java.util.Scanner;
+import java.util.Objects;
 
 public class Point6_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите имя");
-        if (sc.hasNext("Анастасия")) {
-            System.out.println("Я тебя так долго ждал ");
+        Object a = (sc.findInLine("Вася"));
+        Object b = "Вася";
+        Object c = (sc.findInLine("Анастасия"));
+        Object d = "Анастасия";
+        if (Objects.equals(a,b)) {
+            System.out.println("Привет!\nЯ тебя так долго ждал ");
+        }else if (Objects.equals(c,d)){
+            System.out.println("Я тебя так долго ждал");
         }else{
             System.out.println("");
         }
