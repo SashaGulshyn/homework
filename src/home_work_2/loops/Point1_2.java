9654fb0bbf72fe0856d1ef0efedc93c0f575c6e4
Sasha Gulshyn <kessedione@gmail.com>
2023-04-30 17:05:23 +0300

    Добавляю задачу 1.2

---
+++ src/home_work_2/loops/Point1_2.java
@@ -0,0 +1,55 @@
+package src.home_work_2.loops;
+
+import java.util.Scanner;
+
+public class Point1_2 {
+    public static void main(String[] args) {
+        System.out.println("Введите целое число, цифры из которого будут перемножены между собой");
+        Scanner console = new Scanner(System.in);
+        if (!console.hasNextDouble()) {
+            System.out.println("Введено не число");
+        }else{
+           if (console.hasNextDouble() && console.hasNextLong()) {
+               long longFromUser = console.nextLong();
+               long result = 1;
+               long secondSymbol = 1;
+               for (int i = String.valueOf(longFromUser).length(); i > 0; i--) {
+                   secondSymbol = longFromUser % 10;
+                   result = result * secondSymbol;
+                   if (longFromUser / 10 != 0) {
+                       System.out.print(secondSymbol + "*");
+                   } else {
+                       System.out.print(secondSymbol + "=");
+                   }
+                   longFromUser = longFromUser / 10;
+               }
+               System.out.print(result);
+           }else{
+               System.out.println("Введено не целое число");
+            }
+        }
+    }
+}
+
+//int intFromUser = console.nextInt();
+//int int1 = intFromUser % 10;
+//int int2 = intFromUser / 10 % 10;
+//int int3 = intFromUser / 100;
+
+//        int result = 1;
+//        for (int i = 1; i <= intFromUser; i++) {
+//            result = intFromUser * intFromUser;
+//            System.out.print(intFromUser + "*");
+//        }
+//        System.out.println("=" + result);
+
+
+    /*
+    Этот метод проверит, были ли введены юзером числовые значения
+     */
+//public static boolean isNumeric(String str) {
+//for (char c : str.toCharArray()) {
+//if (!Character.isDigit(c)) return false;
+//}
+// return true;
+//}
